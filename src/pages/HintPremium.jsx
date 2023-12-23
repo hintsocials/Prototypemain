59a10d457a1fd0c2c5cd1dfde2dba7a990cea8af
hintsocials <hints.socials@gmail.com>
2023-12-24 00:58:18 +0530

    removed

--- src/pages/HintPremium.jsx
+++ src/pages/HintPremium.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import homegradient from "../assets/Images/homegradient.png";
+
 import BottomNav from "../components/BottomNav";
 import ChatButton from "../assets/icons/Hint.svg";
 import CardList from "../assets/icons/CardList.svg";
