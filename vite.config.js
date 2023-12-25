2a59f62e0bd66e8861b52e46e048b0ec7b9685cf
hintsocials <hints.socials@gmail.com>
2023-12-25 16:15:28 +0530

    host00

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: process.env.PORT || 3000,
-  },
-  resolve: {
-    alias: {
-      '/@/': __dirname,
+export default ({ mode }) => {
+  process.env = process.env || {};
+  const env = Object.keys(process.env).reduce((acc, key) => {
+    acc[`process.env.${key}`] = JSON.stringify(process.env[key]);
+    return acc;
+  }, {});
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      port: process.env.PORT || 3000,
+      host: '0.0.0.0', // Bind to all network interfaces
     },
-  },
-})
+    base: process.env.BASE_URL || '/',
+    resolve: {
+      alias: {
+        '/@/': __dirname,
+      },
+    },
+    define: {
+      ...env,
+    },
+  });
+};
