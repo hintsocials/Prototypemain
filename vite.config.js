928e37506b74ca07230446fb1b7d1b08e357ab83
hintsocials <hints.socials@gmail.com>
2023-12-25 16:05:56 +0530

    processenv

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ const __dirname = dirname(__filename);
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: process.env.PORT || 3000,
   },
   resolve: {
     alias: {
